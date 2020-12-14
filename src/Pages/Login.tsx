299eb61468368543ad50e6c7249f337443df68f7
Eray Chumak <eray_chumak@protonmail.com>
2020-12-14 20:37:09 +0000

    Add login logic

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,7 +1,65 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useCallback, useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 function Login() {
+  const history = useHistory();
+
+  const [email, setEmail] = useState("");
+  const [warnEmail, setWarnEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+  const [warnPass, setWarnPass] = useState("");
+
+  const [warn, setWarn] = useState("");
+
+  useEffect(() => setWarn(""), [email, password]);
+  useEffect(() => setWarnEmail(""), [email]);
+  useEffect(() => setWarnPass(""), [password]);
+
+  const login = useCallback(async () => {
+    if (email.length === 0) {
+      setWarnEmail("Empty");
+    }
+
+    if (password.length < 8) {
+      setWarnPass("Password has to be over 8 chars long.");
+    }
+
+    if (email.length > 0 && password.length > 8) {
+      try {
+        const body: RequestInit = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            email,
+            password
+          })
+        }
+
+        const req = await fetch("/v1/auth/login/", body);
+        const payload = await req.json();
+
+        if (req.status === 401) {
+          setWarn("Incorrect credentials");
+          return;
+        }
+
+        if (req.status === 200) {
+          document.cookie = `accessToken=${payload.access}`;
+          document.cookie = `refreshToken=${payload.refresh}`;
+          history.push("/");
+          return;
+        }
+
+        setWarn(JSON.stringify(payload));
+      } catch (e) {
+        setWarn(`Internal error: ${JSON.stringify(e)}`);
+      }
+    }
+  }, [history, email, password]);
+
   return (
     <div className="page login">
       <h1>Cocktail Hunter</h1>
@@ -21,18 +79,26 @@ function Login() {
         <div className="fields">
           <label>
             <p>Email</p>
-            <input/>
+            {warnEmail.length > 0 && <p>ERROR: {warnEmail}</p>}
+            <input
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
           </label>
           <label>
             <p>Password</p>
-            <input type="password"/>
+            {warnPass.length > 0 && <p>ERROR: {warnPass}</p>}
+            <input
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              type="password"
+            />
           </label>
         </div>
-        <Link to="/inventory">
-          <button>
-            Authenticate
-          </button>
-        </Link>
+        {warn.length > 0 && <p>ERROR: {warn}</p>}
+        <button onClick={login}>
+          Authenticate
+        </button>
         <Link to="/forgot-password">
           <button>
             Forgot Password
