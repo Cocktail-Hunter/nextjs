7a26426c0805876f34bef48ef1b7f5b3ae401a13
Eray Chumak <eray_chumak@protonmail.com>
2020-12-18 19:40:44 +0000

    Fetch user inventory

--- src/Pages/Inventory.tsx
+++ src/Pages/Inventory.tsx
@@ -1,7 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Ingredient, InventoryPayload } from "../interfaces";
 
 function Inventory() {
+  const history = useHistory();
+
+  const [ingredients, setIngredients] = useState<Array<Ingredient>>([]);
+  const [warnIngredients, setWarnIngredients] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    (async () => {
+      const body: RequestInit = {
+        headers: {
+          "Authorization": `Bearer ${accessToken}`
+        }
+      };
+
+      try {
+        const req = await fetch("/v1/user/inventory", body);
+
+        if (req.status === 401) {
+          const { pathname, search } = history.location;
+          const redirect = encodeURIComponent(pathname + search);
+
+          history.push(`/refresh?redirect=${redirect}`);
+          return;
+        }
+
+        if (req.status === 200) {
+          const payload = await req.json() as InventoryPayload;
+          setIngredients(payload.inventory);
+          return;
+        }
+
+        const payload = await req.json();
+        setWarnIngredients(JSON.stringify(payload));
+      } catch (e) {
+        setWarnIngredients(`Internal error: ${JSON.stringify(e)}`);
+      }
+    })();
+  }, [history]);
+
   return (
     <div className="page inventory">
       <h1>Cocktail Hunter</h1>
@@ -18,7 +59,12 @@ function Inventory() {
         </ul>
       </nav>
       <section>
-        <h3>Ingredients</h3>
+        <h3>Inventory</h3>
+        {warnIngredients && <p>{warnIngredients}</p>}
+        {ingredients.length === 0 && <p>Your inventory is currently empty, time to stock up!</p>}
+        {ingredients.map(ingredient => (
+          <p>{ingredient.name}</p>
+        ))}
       </section>
       <section>
         <h3>Cocktails you can make</h3>
