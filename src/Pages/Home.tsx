648243f2f5300f41195a027a82e0736ee98aa430
Eray Chumak <eray_chumak@protonmail.com>
2021-01-01 21:02:04 +0000

    Refactor home page (style + text)

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,74 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import useAuth from "../Hooks/useAuth";
+import React, { FC } from "react";
 
-function Home() {
-  const authed = useAuth();
+import "./Home.scss";
 
-  return (
-    <div className="page home">
-      <h1>Cocktail Hunter</h1>
-      <p>Find cocktails you can make based on your inventory.</p>
-      <nav>
-        {authed && (
-          <ul>
-            <li>
-              <Link to="/inventory">Inventory</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-          </ul>
-        )}
-        {!authed && (
-          <ul>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </ul>
-        )}
-      </nav>
-      <section>
-        <h2>Context</h2>
-        <p>
-          People sometimes don't have a wide variety of alcohol in their
-          homes and might not want to go all the way to the store for that
-          one mysterious ingredient.
-        </p>
-      </section>
-      <section>
-        <h2>This is where this app comes in...</h2>
-        <p>
-          Users will be able to input a list of all the alcohol and ingredients
-          they've got and the app will return all possible cocktails the user
-          can make. By default, it will result with cocktails strictly with the
-          ingredients the user has but it can be toggled off where the app can
-          result with cocktails with 1-2 missing ingredients.
-        </p>
-      </section>
-      <section>
-        <h2>Long term plans</h2>
-        <p>
-          Users are able to publish their own cocktail recipes that they've
-          discovered or want to share with the community.
-        </p>
-      </section>
-      <footer>
-        <hr/>
-        <ul>
-          <li>
-            <Link to="/tos">Terms of Service</Link>
-          </li>
-          <li>
-            <Link to="/privacy policy">Privacy Policy</Link>
-          </li>
-        </ul>
-      </footer>
-    </div>
-  );
-}
+const Home: FC = () => (
+  <div className="page home">
+    <h2>Hello,</h2>
+    <section>
+      <h1>Start making cocktails now</h1>
+      <p>
+        Add ingredients to your inventory and see a list of cocktails
+        you can make using them at home or even at the bar.
+      </p>
+    </section>
+    <section>
+      <h1>Manage your inventory of ingredients</h1>
+      <p>
+        You can add and remove as many ingredients as you like from your inventory
+        and you can control your flexibility to allow you to see cocktails you can
+        make even if you're missing 1 or 2 ingredients.
+      </p>
+    </section>
+    <section>
+      <h1>Make and publish your own cocktails</h1>
+      <p>
+        You can create your own cocktails and share with everyone else by making it
+        public, once your cocktail is approved, it will appear in everyone else's
+        results if they have your ingredients and that it matches their flexibility!
+      </p>
+    </section>
+  </div>
+);
 
 export default Home;
