e3d31e209bfe52c9a51ea96f202ba43f910f7682
Eray Chumak <eray_chumak@protonmail.com>
2021-01-25 01:41:03 +0000

    Add cocktail and ingredient sector states

--- src/Contexts/Inventory.tsx
+++ src/Contexts/Inventory.tsx
@@ -1,13 +1,17 @@
 import {createContext} from "react";
-import { ESector, IIngredient } from "../interfaces";
+import { ESector, ICocktail, IIngredient } from "../interfaces";
 
 export interface InventoryContextProps {
-  sector: ESector,
-  setSector: React.Dispatch<React.SetStateAction<ESector>>,
+  ingredientSector: ESector,
+  setIngredientSector: React.Dispatch<React.SetStateAction<ESector>>,
+  cocktailSector: ESector,
+  setCocktailSector: React.Dispatch<React.SetStateAction<ESector>>,
   ingredientsList: IIngredient[],
   setIngredientsList: React.Dispatch<React.SetStateAction<IIngredient[]>>,
   inventory: IIngredient[],
-  setInventory: React.Dispatch<React.SetStateAction<IIngredient[]>>
+  setInventory: React.Dispatch<React.SetStateAction<IIngredient[]>>,
+  cocktailsList: ICocktail[],
+  setCocktailsList: React.Dispatch<React.SetStateAction<ICocktail[]>>
 }
 
 export const InventoryContext = createContext<InventoryContextProps | null>(null);
--- src/Pages/Inventory/Index.tsx
+++ src/Pages/Inventory/Index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { ESector, ICocktail, IIngredient } from "../../interfaces";
-import Cocktails from "./Cocktails";
+import Cocktails from "./Cocktails/Index";
 import AddNewIngredient from "./AddNewIngredient/Index";
 import InventoryIngredients from "./InventoryIngredients";
 import { InventoryContext } from "../../Contexts/Inventory";
@@ -8,31 +8,33 @@ import { InventoryContext } from "../../Contexts/Inventory";
 import "./Index.scss";
 
 function Inventory() {
-  const [sector, setSector] = useState<ESector>(ESector.PUBLIC);
+  const [ingredientSector, setIngredientSector] = useState<ESector>(ESector.PUBLIC);
+  const [cocktailSector, setCocktailSector] = useState<ESector>(ESector.PUBLIC);
   const [ingredientsList, setIngredientsList] = useState<Array<IIngredient>>([]);
   const [cocktailsList, setCocktailsList] = useState<Array<ICocktail>>([]);
   const [inventory, setInventory] = useState<Array<IIngredient>>([]);
 
+  const initialValues = {
+    ingredientSector, setIngredientSector,
+    cocktailSector, setCocktailSector,
+    ingredientsList, setIngredientsList,
+    inventory, setInventory,
+    cocktailsList, setCocktailsList
+  };
+
   return (
-    <InventoryContext.Provider value={{sector, setSector, ingredientsList, setIngredientsList, inventory, setInventory}}>
+    <InventoryContext.Provider value={initialValues}>
       <div className="page inventory">
         <section className="inventorySection">
           <h1>You have <span className="highlight">{inventory.length}</span> ingredients</h1>
           <AddNewIngredient/>
           <InventoryIngredients/>
         </section>
-        {/* <div className="separator"/>
+        {/* <div className="separator"/> */}
         <section>
           <h1>Cocktails you can make</h1>
-          <Cocktails
-            cocktailsList={cocktailsList}
-            setCocktailsList={setCocktailsList}
-            ingredientsList={ingredientsList}
-            setIngredientsList={setIngredientsList}
-            inventory={inventory}
-            setInventory={setInventory}
-          />
-        </section> */}
+          <Cocktails/>
+        </section>
       </div>
     </InventoryContext.Provider>
   );
