96122662173064e6932cb79295cd9ffd1194b4d9
Eray Chumak <eray_chumak@protonmail.com>
2021-01-25 02:43:27 +0000

    Add Trash icon component

---
+++ src/assets/Icons/Trash.tsx
@@ -0,0 +1,10 @@
+const Trash = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="22" viewBox="0 0 20 22">
+    <g id="trash" transform="translate(-2 -1)">
+      <path id="Path_37" data-name="Path 37" d="M3,6H21" fill="none" stroke="#eee" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+      <path id="Path_38" data-name="Path 38" d="M19,6V20a2,2,0,0,1-2,2H7a2,2,0,0,1-2-2V6M8,6V4a2,2,0,0,1,2-2h4a2,2,0,0,1,2,2V6" fill="none" stroke="#eee" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+    </g>
+  </svg>
+);
+
+export default Trash;
