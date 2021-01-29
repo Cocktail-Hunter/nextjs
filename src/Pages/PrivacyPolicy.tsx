9e04553de52d20773a07f78f2cc67b6707182bc1
Eray Chumak <eray_chumak@protonmail.com>
2021-01-29 19:11:51 +0000

    Add note

--- src/Pages/PrivacyPolicy.tsx
+++ src/Pages/PrivacyPolicy.tsx
@@ -1,23 +1,9 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { FC } from "react";
 
-function PrivacyPolicy() {
+const PrivacyPolicy: FC = () => {
   return (
     <div className="page privacyPolicy">
-      <h1>Cocktail Hunter</h1>
-      <p>Find cocktails you can make based on your inventory.</p>
-      <h2>Privacy Policy</h2>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/tos">Terms of Services</Link>
-          </li>
-        </ul>
-      </nav>
-      <p>Being drafted.</p>
+      <h2>To be written</h2>
     </div>
   );
 }
